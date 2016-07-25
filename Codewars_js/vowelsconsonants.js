00a778e79b0e0f1d4e33cad9dc82599cb8d2cfde
Iggi <iggirex@gmail.com>
2016-07-25 15:19:47 -0600

    advice for Oz

--- Codewars_js/vowelsconsonants.js
+++ Codewars_js/vowelsconsonants.js
@@ -1,24 +1,24 @@
 function getCount(words) {
     var result = {vowels: 0, consonants: 0};
-     
+
     if (typeof words !== 'string' || typeof words === null || words === '' || words === undefined){
-        
+
         return result;
-    
+
     }
-    
+
     words = words.replace( /[^a-zA-Z]/, "");
     words = words.trim();
 
     if (words.match(/[bcdfghjklmnpqrstvwxyz]/gi) === null && words.match(/[aeiou]/gi) === null){
-    
+
         console.log(words);
         return result;
-    
+
     } else if (words.match(/[aeiou]/gi) === null) {
 
         result.vowels = 0;
-        result.consonants = words.match(/[bcdfghjklmnpqrstvwxyz]/gi).length; 
+        result.consonants = words.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
         console.log(words);
         return result;
 
@@ -32,9 +32,11 @@ function getCount(words) {
     } else {
 
         result.vowels = words.match(/[aeiou]/gi).length;
-        result.consonants = words.match(/[bcdfghjklmnpqrstvwxyz]/gi).length; 
+        result.consonants = words.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
         console.log(words);
         return result;
 
     }
 }
+
+\\turn if statements into functions
